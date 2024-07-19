725acdd2db526a4d7e251d00b6b1863ea8453a7a
denismnjima <denismnjima@gmail.com>
2024-07-19 17:03:39 +0300

    solved question

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code here
+
+class Breakfast {
+    constructor(food, drink) {
+      this.food = food;
+      this.drink = drink;
+    }
+  }
+  
+  class Lunch {
+    constructor(salad, soup, drink) {
+      this.salad = salad;
+      this.soup = soup;
+      this.drink = drink;
+    }
+  }
+  
+  class Dinner {
+    constructor(salad, soup, entree, dessert) {
+      this.salad = salad;
+      this.soup = soup;
+      this.entree = entree;
+      // Dessert is a private field, can't be directly tested
+      this._dessert = dessert;
+    }
+  }
+  
+  // Export the classes for testing
+  module.exports = { Breakfast, Lunch, Dinner };
+  
